3717bc9f0812d5dbf614c4237c38b8a9bc5e8716
@Hamza10 <54814111+hamzaanis10@users.noreply.github.com>
2022-10-07 12:29:43 +0500

    Add files via upload

---
+++ regexp and is null.sql
@@ -0,0 +1,12 @@
+/*
+Regular expressions
+
+^ indicates beginning of a string
+$ indicates end of a string
+| logical OR pipe same as OR operator
+[abcd] match exactly the these chararcters that are in the bracket
+[a-h] matches the range between a to h 
+
+*/
+
+select * from customers where phone is  not null;
