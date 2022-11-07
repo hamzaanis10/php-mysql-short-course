0ea60ec1b7ba6c7dcd0da186e90727e4092855ba
@Hamza10 <54814111+hamzaanis10@users.noreply.github.com>
2022-11-07 12:28:16 +0500

    Add files via upload

---
+++ arrayMethods.php
@@ -0,0 +1,73 @@
+<?php
+
+$fruits = ['apple', 'mango', 'banana', 'strawberry', 'peach', 'watermelon'];
+
+// Get length 
+// echo count($fruits);
+
+// Add to array
+// $fruits[] = 'grape';
+// $fruits[] = 'strawberry';
+
+// print_r($fruits);
+
+// Add element to the last
+// array_push($fruits, 'blueberry', 'strawberry');
+
+// Add element to the beginning
+// array_unshift($fruits, 'peach');
+
+
+
+// Remove from array
+// array_pop($fruits);
+
+// array_shift($fruits);
+
+// $chunked_array = array_chunk($fruits, 4);
+
+// print_r($chunked_array);
+
+// $arr1 = [1, 2, 3];
+// $arr2 = [4, 5, 6];
+
+// $arr3 = array_merge($arr1, $arr2);
+// print_r($arr3);
+
+// Spread Operator
+
+// $arr4 = [...$arr1, ...$arr2];
+// print_r($arr4);
+
+
+$a = ['green', 'red', 'yellow'];
+$b = ['banana', 'apple', 'mango'];
+
+
+
+// $c = array_combine($a, $b);
+// print_r($c);
+
+// $flipped = array_flip($c);
+// print_r($flipped);
+
+// print_r($c);
+
+// $numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// // Array map 
+// $newNumbers = array_map(function ($number) {
+//     return "Number $number";
+// }, $numbers);
+
+// print_r($newNumbers);
+
+// Array filter
+
+// $evenNumbers = array_filter($numbers, fn ($number) => $number % 2 === 0);
+// print_r($evenNumbers);
+
+// array reduce
+
+// $sum = array_reduce($numbers, fn ($carry, $number) => $carry + $number);
+// print_r($sum);
